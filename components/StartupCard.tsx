bcf10e3ec924c555c59db4d38da3de9a05fc62bf
MUHANNAD SALKINI <mohanad.salkini@gmail.com>
2024-11-01 12:26:40 +0300

    Added create startup form

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -33,7 +33,7 @@ const StartupCard = ({ post }: { post: StartupCardType }) => {
       <div className="flex-between mt-5 gap-5">
         <div className="flex-1">
           <Link href={`/user/${author?._id}`}>
-            <p className="text-16-medium line-clamp-1">{author?._id}</p>
+            <p className="text-16-medium line-clamp-1">{author?.name}</p>
           </Link>
           <Link href={`/startup/${_id}`}>
             <h3 className="text-26-semibold">{title}</h3>
